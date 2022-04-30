33a205210b9b79117c8dde7263d68b073d1263a6
C <caaarloos300@gmail.com>
2022-04-30 23:14:46 +0200

    updates

--- flask/main.py
+++ flask/main.py
@@ -8,11 +8,13 @@ async def room():
     with open('../sample_details.json') as json_file:
         data = json.load(json_file)
     return data["room"]
+
 @app.get("/score")
 async def score():
     with open('../sample_details.json') as json_file:
         data = json.load(json_file)
     return data["score"]
+    
 @app.get("/objects")
 async def objects():
     with open('../sample_details.json') as json_file:
