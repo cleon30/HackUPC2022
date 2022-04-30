147cbfe49ae20c7ba22f2cc7872d5c5e052f1091
C <caaarloos300@gmail.com>
2022-04-30 05:17:58 +0200

    updates

---
+++ main.py
@@ -0,0 +1,3 @@
+from vision import prediction
+path = 'https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_inbody_900x506/public/image/2016/08/main/luxurious-master-bedroom-sun-1114.jpg'
+prediction(path)
--- vision.py
+++ vision.py
@@ -1,25 +1,25 @@
 import requests
 import json
+def prediction(s):
+    url = 'https://api-us.restb.ai/vision/v2/multipredict'
+    payload = {
+        # Add your client key
+        'client_key': '18d2c3def97f08eee4261f6dbe69c2081a4535166e48f116d38c17303f5b3a39',
+        'model_id': 're_roomtype_global_v2,re_features_v3,re_appliances_v2,re_condition',
+        # Add the image URL you want to classify
+        'image_url': s
+    }
 
-url = 'https://api-us.restb.ai/vision/v2/multipredict'
-payload = {
-    # Add your client key
-    'client_key': '18d2c3def97f08eee4261f6dbe69c2081a4535166e48f116d38c17303f5b3a39',
-    'model_id': 're_roomtype_global_v2,re_features_v3,re_appliances_v2,re_condition',
-    # Add the image URL you want to classify
-    'image_url': 'https://es.habcdn.com/photos/business/medium/img-1686-1-1652143.jpg'
-}
+    # Make the classify request
+    response = requests.get(url, params=payload)
 
-# Make the classify request
-response = requests.get(url, params=payload)
-
-# The response is formatted in JSON
-json_response = response.json()
-print(json_response['response']['solutions']['re_roomtype_global_v2']['top_prediction']['label'])
-print(json_response['response']['solutions']['re_condition']['score'])
+    # The response is formatted in JSON
+    json_response = response.json()
+    print(json_response['response']['solutions']['re_roomtype_global_v2']['top_prediction']['label'])
+    print(json_response['response']['solutions']['re_condition']['score'])
 
 
-json_object = json.dumps(json_response, indent = 4)
-with open("sample.json", "w") as outfile:
-    outfile.write(json_object)
-#print(json_object[1])
+    json_object = json.dumps(json_response, indent = 4)
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
+    #print(json_object[1])
