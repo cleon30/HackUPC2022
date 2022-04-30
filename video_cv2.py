c1e89c3a9f9c3a9b7828a35a232b3b8653ab423c
C <caaarloos300@gmail.com>
2022-04-30 15:31:57 +0200

    updates

--- video_cv2.py
+++ video_cv2.py
@@ -1,17 +1,24 @@
+from tempfile import NamedTemporaryFile
 import pydrive
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import cv2
 import numpy as np
+
+from vision import prediction
 i = 0
 cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
     cv2.imshow('frame',frame)
-    cv2.imwrite('Frame'+str(i)+'.jpg', frame)
+    file = NamedTemporaryFile(suffix=".jpg",prefix="./frame_",delete=True)
+    cv2.imwrite(file.name, frame)
+    ret = prediction(file.name)
+    print(ret)
+    file.close()
     i += 1
-    if cv2.waitKey(5000) & 0xFF == ord('q'):
+    if cv2.waitKey(2000) & 0xFF == ord('q'):
         
         break
 
